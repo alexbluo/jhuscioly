1a2d7ce39180fe758db694d6186bedac4bfd0568
Alex Luo <alexbluo01@gmail.com>
2022-06-19 16:06:30 -0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./components/*.{js,jsx}", "./pages/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
