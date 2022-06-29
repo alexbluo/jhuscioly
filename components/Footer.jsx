3e5097fa0d1ee5f36ff9a639708f14f962dfe2e1
Daniel Jin <97412514+danielsunjin@users.noreply.github.com>
2022-06-29 18:06:28 -0400

    updated footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,17 +4,15 @@ import { FaInstagram, FaEnvelope } from "react-icons/fa";
 // TODO: content
 function Footer() {
   return (
-    <footer className="h-64 w-screen bg-blue-dark p-8 text-sm text-white">
-      <div className="flex h-full flex-col">
-        <div className="flex h-full justify-between gap-8">
-          <div>
-            <a>
-              <FaInstagram />
-            </a>
-            <a href="mailto: jhso.invite@gmail.com">
-              <FaEnvelope />
-            </a>
-          </div>
+    <footer className="bg-blue-dark p-8 text-white">
+      <div className="flex flex-col gap-8">
+        <div className="flex gap-8 text-xl">
+          <a>
+            <FaInstagram />
+          </a>
+          <a href="mailto: jhso.invite@gmail.com">
+            <FaEnvelope />
+          </a>
         </div>
         <p>Copyright © 2022 JHSOI. All rights reserved.</p>
       </div>
