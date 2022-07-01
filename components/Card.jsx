21d794b1c92a90b0adb3ba64cbe7013756bdcb2b
Daniel Jin <97412514+danielsunjin@users.noreply.github.com>
2022-06-30 21:04:08 -0400

    animated cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,12 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Card({ url, name, title }) {
   return (
-    <div className="rounded-lg bg-blue-dark text-center">
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      className="rounded-lg bg-gradient-to-r from-blue-dark to-blue-light text-center"
+    >
       <div className="relative aspect-square w-full">
         <Image
           className="rounded-t-lg"
@@ -14,6 +18,6 @@ export default function Card({ url, name, title }) {
       </div>
       <h2 className="mt-2 text-xl font-semibold text-white">{name}</h2>
       <h2 className="text-md mb-2 text-white">{title}</h2>
-    </div>
+    </motion.div>
   );
 }
